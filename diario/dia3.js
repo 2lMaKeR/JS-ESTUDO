e871f82f252c93591804adab633fe89789a70e94
Felipe Tomé or 2lMaKeR <neoswar999@gmail.com>
2023-01-17 21:22:38 -0300

    dia 3

---
+++ diario/dia3.js
@@ -0,0 +1,13 @@
+
+// chamando e utilizando o terminal JS
+// começando com a nossa querida mensagem do terminal 
+console.log('Olá eu sou o terminal!!')
+console.log('e hoje iremos ver, if e else, famosos e SE e SE NÃO.')
+// por exemplo vamos definir uma velocidade.
+var vel = 12  // em Km/h por favor
+console.log(`Sua velocidade é de ${vel}Km/h.`)
+if(vel>60) { // traduzindo a codificação
+// se a velocidade é maior que 60, aparece a mensagem 'MULTADO!!!'
+    console.log(`MULTADO!!!`)
+}else console.log(`Tenha uma boa viagem!`)
+//se não, aparece a mensagem 'Tenha uma boa viagem'.
