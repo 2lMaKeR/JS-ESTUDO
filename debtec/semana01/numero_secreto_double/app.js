13e1ff84c2f4519955291edda4f18bc824094a9f
Felipe Tomé or 2lMaKeR <120343427+2lMaKeR@users.noreply.github.com>
2024-07-01 20:28:57 -0300

    estudo novo adicionado

---
+++ debtec/semana01/numero_secreto_double/app.js
@@ -0,0 +1,11 @@
+function exibirTextoNaTela(tag,texto){
+    let campo=document.querySelector(tag);
+    campo.innerHTML= texto;
+};
+
+exibirTextoNaTela('h1','Jogo do número secreto');
+exibirTextoNaTela('p','Escolha um número entre 1 e 10');
+
+function verificarChute(){
+
+};
